3aae2a2f78af3a1e783d971d2e3f31e76e22cba1
BuildTools <unconfigured@null.spigotmc.org>
2016-02-18 17:29:41 -0800

    I doubt this will need updating
    All it needs is tampermonkey

---
+++ skip.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         skip.js
+// @namespace    JasorYEH
+// @version      1.0
+// @description  Skip Brain Dare's waiting time!
+// @author       JasorYEH
+// @match        https://gist.github.com/search?utf8=%E2%9C%93&q=tampermonkey&ref=searchresults
+// @grant        none
+// @include      http://www.braindare.com/solution/*
+// ==/UserScript==
+/* jshint -W097 */
+'use strict';
+
+// Your code here...
+
+console.log("Thank You and Please check out my YouTube Channel at youtube.com/JasorYEH")
+console.log("Your script is now running")
+
+function checkFor(counter){
+    return checkFor;
+}
+for(counter !== 0; counter--; null){
+    console.log("Number is currenty " + counter);
+}
